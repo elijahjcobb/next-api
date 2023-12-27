8beb7eced3d82376909f7b9cef7384b1ce111b4b
Elijah Cobb <elijah@elijahcobb.com>
2023-12-27 13:47:05 -0500

    update hash

--- src/hash.ts
+++ src/hash.ts
@@ -1,9 +1,8 @@
-import { createHash } from "node:crypto";
-
-const ALGO = "sha256";
-
-export function hash(value: string): string {
-  const hasher = createHash(ALGO);
-  hasher.update(value);
-  return hasher.digest("hex");
+export async function hash(value: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(value);
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return Array.from(new Uint8Array(hash))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
 }
